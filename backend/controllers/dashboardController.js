cee46cc85e48d5f77098e958c2e49dbc695bf572
ArulKumaran-hub <mukunthanarulkumaran10@gmail.com>
2026-09-04 12:09:30 +0530

    Added my part

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,6 +1,8 @@
 const Report = require('../models/Report');
 
+
 // GET /api/dashboard/stats
+//testing
 exports.getDashboardStats = async (req, res) => {
   try {
     const totalReports = await Report.countDocuments();
